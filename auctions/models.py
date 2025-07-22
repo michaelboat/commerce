986aaa1b6c2137d977fe1fe1aa478a3cfd937d3c
Michael Boateng <mboateng@colgate.edu>
2025-07-22 12:26:10 -0400

    added listing model

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,16 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    categoryName = models.CharField(max_length=50)
+
+class Listing(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=300)
+    imageURL =  models.CharField(max_length=1000)
+    price = models.FloatField()
+    isAction = models.BooleanField(default=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE , blank = True, related_name="user")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category")
+    
